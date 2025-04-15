10a8bd506a2b5fcdb87da4726037fbc3b2226457
zaRu-bit <84655499+zaRu-bit@users.noreply.github.com>
2025-04-16 00:16:17 +1000

    Mess around with Routes to make chat the default view now and if the user is not logged in it will default to the login screen

--- frontend/sight/src/App.jsx
+++ frontend/sight/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={<Chat />} />
